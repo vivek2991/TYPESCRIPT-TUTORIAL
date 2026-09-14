98afb9c522b718381ff5216fc8ba923e2728426e
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-14 14:36:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+var userName = "Vivek Patel";
+var age = 20;
+var college = "IIM Ahmedabad";
+console.log(userName);
+console.log(age);
+console.log(college);
---
+++ app.ts
@@ -0,0 +1,12 @@
+var userName:string = "Vivek Patel"
+
+var age:number = 20
+
+var college:string = "IIM Ahmedabad"
+
+  console.log(userName);
+  console.log(age);
+  console.log(college);
+  
+  
+  
