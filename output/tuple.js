ca2dd7dad5a6a02f5f54752fd50fbdb82794deec
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-21 14:15:12 +0530

    tuple in TypeScript

---
+++ output/tuple.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var emp = ['Vivek', 35, true];
+console.log(emp);
+//# sourceMappingURL=tuple.js.map
---
+++ tuple.ts
@@ -0,0 +1,7 @@
+var emp:[string, number, boolean] = ['Vivek', 35, true]
+
+
+console.log(emp);
+
+
+
