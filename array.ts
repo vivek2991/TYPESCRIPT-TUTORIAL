7651360210d2df761a79a4e1d7420a3b81cd101c
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-21 14:06:56 +0530

    array in TypeScript

---
+++ array.ts
@@ -0,0 +1,16 @@
+var users:string[] = ['vivek', 'Smriti', 'Shreyanka']
+
+console.log(users);
+
+var marks:number[] = [98, 99, 95, 90];
+
+console.log(marks);
+
+
+var students:Array<string>= ['vivek', 'Smriti']
+students.push('shreyanka')
+
+console.log(students);
+
+var collegeName:ReadonlyArray<string> = ['IIM', 'IIT', 'IET']
+
---
+++ output/array.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var users = ['vivek', 'Smriti', 'Shreyanka'];
+console.log(users);
+var marks = [98, 99, 95, 90];
+console.log(marks);
+var students = ['vivek', 'Smriti'];
+students.push('shreyanka');
+console.log(students);
+var collegeName = ['IIM', 'IIT', 'IET'];
+//# sourceMappingURL=array.js.map
