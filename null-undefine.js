ec2beee4e49ad0c917d377becb667da99ef52ecb
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-21 12:21:17 +0530

    null and undefined

---
+++ null-undefine.js
@@ -0,0 +1,14 @@
+"use strict";
+var data = null;
+console.log(data);
+var item1 = "";
+console.log(item1);
+var val = undefined;
+console.log(val);
+var userName = null;
+var login = true;
+if (login) {
+    userName = "vivek patel";
+}
+console.log(userName);
+console.log(typeof data);
---
+++ null-undefine.ts
@@ -0,0 +1,27 @@
+var data:null = null;
+
+console.log(data);
+
+var item1 = "";
+
+console.log(item1);
+
+var val:undefined = undefined;
+
+console.log(val);
+
+
+var userName : null | string = null;
+var login = true;
+
+if(login){
+    userName = "vivek patel"
+}
+
+console.log(userName);
+console.log(typeof data);
+
+
+
+
+
