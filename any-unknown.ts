2b40364c3e062c5f01a7b707e65eabc998501d08
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-22 14:49:13 +0530

    any unknown in TypeScript

---
+++ any-unknown.ts
@@ -0,0 +1,25 @@
+var value:any = "Vivek";
+
+console.log(value);
+
+value = 100;
+
+console.log(value);
+
+value = true
+
+console.log(value);
+
+value = {}
+
+var value1:unknown = "Smriti"
+
+value1 = 100;
+
+value1 = ['vk']
+value1 = {}
+value1 = "Sm vk"
+
+if(typeof value1=='string'){
+    console.log(value1.toUpperCase());
+}
---
+++ output/any-unknown.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var value = "Vivek";
+console.log(value);
+value = 100;
+console.log(value);
+value = true;
+console.log(value);
+value = {};
+var value1 = "Smriti";
+value1 = 100;
+value1 = ['vk'];
+value1 = {};
+value1 = "Sm vk";
+if (typeof value1 == 'string') {
+    console.log(value1.toUpperCase());
+}
+//# sourceMappingURL=any-unknown.js.map
