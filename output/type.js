c823694092e28aefcad49682cf7936016e268cdf
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-26 11:00:54 +0530

    type in TypeScript

---
+++ output/type.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var empData = {
+    name: 'Vivek',
+    email: 'vivek@gmail.com'
+};
+console.log(empData);
+var studentData = {
+    name: 'Smriti',
+    email: 'smriti@gmail.com'
+};
+console.log(studentData);
+//# sourceMappingURL=type.js.map
---
+++ type.ts
@@ -0,0 +1,16 @@
+type DataType = {name:string, email: string}
+
+var empData: DataType = {
+    name: 'Vivek',
+    email: 'vivek@gmail.com'
+}
+
+console.log(empData);
+
+
+var studentData : DataType = {
+    name: 'Smriti',
+    email: 'smriti@gmail.com'
+}
+
+console.log(studentData);
