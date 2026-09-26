8debb8a1739a818215c1a2d54f88af669ae7dd52
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-26 12:24:04 +0530

    Inheritance in TypeScript

---
+++ inheritance.ts
@@ -0,0 +1,35 @@
+class Auth{
+    login(name:string, password:string){
+        if(name && password){
+            return "Login Successfully"
+        } else {
+            return "Not Login"
+        }
+    }
+}
+
+class Student extends Auth{
+
+    result(marks:number){
+        if(marks >35){
+            return "Pass"
+        } else {
+            return "Failed"
+        }
+    }
+}
+
+var s1 = new Student();
+console.log(s1.login('vivek', '12345'));
+console.log(s1.result(60));
+
+class Teacher extends Auth{
+    subject(sub:string){
+        return "They Teach: "+sub; 
+    }
+}
+
+var t1 = new Teacher();
+console.log(t1.login('vk', '123'));
+console.log(t1.subject('Math'));
+
---
+++ output/inheritance.js
@@ -0,0 +1,32 @@
+class Auth {
+    login(name, password) {
+        if (name && password) {
+            return "Login Successfully";
+        }
+        else {
+            return "Not Login";
+        }
+    }
+}
+class Student extends Auth {
+    result(marks) {
+        if (marks > 35) {
+            return "Pass";
+        }
+        else {
+            return "Failed";
+        }
+    }
+}
+var s1 = new Student();
+console.log(s1.login('vivek', '12345'));
+console.log(s1.result(60));
+class Teacher extends Auth {
+    subject(sub) {
+        return "They Teach: " + sub;
+    }
+}
+var t1 = new Teacher();
+console.log(t1.login('vk', '123'));
+console.log(t1.subject('Math'));
+export {};
