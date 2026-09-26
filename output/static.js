2ad84a9d48bb66096cb2090997910a2c0435cf44
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-26 13:40:53 +0530

    static in typescript

---
+++ output/static.js
@@ -0,0 +1,11 @@
+class Company {
+    static name = 'Google';
+    static getName() {
+        return "Microsoft";
+    }
+}
+//var c1 = new Company();
+//console.log(c1.name);
+console.log(Company.name);
+console.log(Company.getName());
+export {};
---
+++ static.ts
@@ -0,0 +1,15 @@
+class Company{
+    static name:string = 'Google';
+
+    static getName(){
+        return "Microsoft"
+    }
+}
+
+//var c1 = new Company();
+//console.log(c1.name);
+
+console.log(Company.name);
+console.log(Company.getName());
+
+
